5ce1ea1aaab4e096eea629d8f4897a52172a9adf
aberzzotti <71133661+aberzzotti@users.noreply.github.com>
2023-04-02 02:25:15 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import MySQLdb
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,session
 
 from utilities.mail import sendEmail
 from utilities.scrap_argprop import scrapingArgenProp
 from utilities.scrap_clarinprop import scrapingClarinProp
 from utilities.properati import properatiWebScraping
 app = Flask(__name__)
+app.secret_key = "vivi"
 
 
 @app.route('/')
@@ -17,14 +18,10 @@ def index():
 def menu():
   return render_template('index.html')
 
-
-@app.route('/mudanza')
-def mudanza():
-  return render_template('mudanza.html')
-
 @app.route('/crearUsuario', methods=['GET', 'POST'])
 def crearUsuario():
   if (request.method == "POST"):
+    session['usuario'] = str(request.form['nombre'])
     return render_template("pagina.html")
   else:
     return render_template("crearUsuario.html")
@@ -33,6 +30,8 @@ def crearUsuario():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if (request.method == "POST"):
+    session['usuario'] = str(request.form['nombre'])
+    print(session['usuario'])
     return render_template("pagina.html")
   else:
     return render_template("login.html")
@@ -48,14 +47,21 @@ def barrios():
 
   return "HOLA"
 
+@app.route('/user', methods=['POST'])
+def user():
+ 
+
+  return render_template("pagina.html")
+
 @app.route('/mail', methods=['POST'])
 def mail():
     cantAmb = str(request.form["cant"])
     barrio = request.form["barrio"]
     precioMin = str(request.form["min"])
     precioMax = str(request.form["max"])
-    email = "tomasacurti@gmail.com"
-    user = "Antonella" + " " + "Berzzotti"
+    print(session['usuario'])
+    email = str(session['usuario'])
+    user = str(session['usuario']) 
     resultsProperati = properatiWebScraping(cantAmb, precioMin, precioMax, barrio)
     resultsArgen = scrapingArgenProp(cantAmb, barrio, precioMin, precioMax)
     resultsClarin = scrapingClarinProp(cantAmb, barrio, precioMin, precioMax)
