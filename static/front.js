89728139466f1a6da8f3811ce1a3dbf3d054fa96
aberzzotti <71133661+aberzzotti@users.noreply.github.com>
2023-04-02 04:40:04 -0300

    Add files via upload

--- static/front.js
+++ static/front.js
@@ -1,23 +1,4 @@
 
-function cargarSelect() {
-
-  $.ajax({
-    url:"/barrios",
-    type:"GET",
-    success: function(response) {
-      console.log(response)  
-      let tabla = "";
-          for (let i = 0; i < response.length; i++) {
-          tabla += '<option>' + response[i][0] + '</option>'
-      }
-      document.getElementById("select").innerHTML += tabla;
-    },
-    error: function(error){
-      console.log(error)
-    }
-  })
-
-}
 function Enviar(){
   
    let barrio=document.getElementById("barrio").value
